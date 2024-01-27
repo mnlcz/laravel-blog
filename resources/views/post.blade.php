ae5a6c7754741bf5afa057f55f6bd6dc7376544a
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-27 00:19:26 -0300

    Init repo at Episode 8
    Ep 8: store blog posts as HTML files

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <?= $post ?>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum sed non possimus, iure numquam reiciendis
+            consequuntur aliquid quos doloremque consectetur odit necessitatibus ex voluptatem quisquam, ad ipsum,
+            placeat fuga incidunt modi commodi beatae quod. Tempore architecto vitae nisi facilis officia sint, vero
+            expedita qui odit maiores dolor nam accusamus aspernatur.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eum eos dignissimos voluptatem a maxime
+            possimus molestiae illum vero voluptates debitis, ratione perspiciatis natus sequi esse labore ipsam
+            distinctio et rerum error accusamus ipsa maiores iusto eligendi. In inventore ab rem voluptatibus dolore
+            laboriosam repellendus adipisci obcaecati libero, quibusdam veniam eius numquam totam nam, perspiciatis
+            praesentium qui aut ea iusto. Id reiciendis omnis rerum veritatis temporibus maiores animi in aperiam
+            dignissimos culpa ab repellat earum est vitae voluptates hic ratione laudantium rem consequatur adipisci
+            corrupti iste reprehenderit, quas dolore. Ipsum dolore culpa nemo? Libero quisquam impedit deleniti quis
+            fuga quibusdam error pariatur? Nobis tempore, saepe unde maxime laboriosam vel, eveniet debitis omnis
+            similique ea mollitia voluptates obcaecati reiciendis numquam dolorum eius accusamus, nemo voluptas labore
+            quo consequuntur dolores? Cumque fuga rerum natus exercitationem nisi, laudantium alias tempore quae
+            cupiditate itaque ullam quaerat sequi delectus velit, est iste quia. Reiciendis asperiores ab illum
+            recusandae laboriosam inventore tenetur.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium incidunt omnis recusandae nulla autem
+            minus id
+            sapiente, animi, quaerat qui quam nostrum pariatur dignissimos consequatur, laborum maiores repudiandae
+            nobis nemo
+            vero non assumenda nesciunt quo accusamus. Tenetur harum facere quae pariatur. Laboriosam commodi, iste
+            error ipsum
+            provident dolor maxime nam. Dignissimos, laudantium. Ab alias officiis sed? Modi harum dignissimos eius
+            dolor,
+            reiciendis dolore maxime qui laborum ab deleniti quas voluptas molestias cum explicabo nobis eos nisi,
+            aliquid vel
+            architecto velit.
+        </p>
+    </article>
+</body>
